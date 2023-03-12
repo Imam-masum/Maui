3e0a7a27f074dc5e2e445e7ff9a4897eaf105a6e
Imam <imammasum.biu@gmail.com>
2023-03-12 14:22:06 +0600

    g

---
+++ Maui_To_Do/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+
+
+namespace Maui_To_Do.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    string text;
+
+    //[ICommand]
+
+    void Add()
+    {
+        Text = string.Empty;
+    }
+}
